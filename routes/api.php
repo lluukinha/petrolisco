c9b6e877950713f7304b635b9e0f9447398742d7
Lucas <lucas.prog07@gmail.com>
2021-11-20 12:43:46 -0300

    Login, auth and JWT

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,14 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('api')->prefix('auth')->group(function () {
+    Route::post('login', [AuthController::class, 'login'])->name('auth.login');
+    Route::post('logout', [AuthController::class, 'logout'])->name('auth.logout');
+    Route::post('refresh', [AuthController::class, 'refresh'])->name('auth.refresh');
+    Route::get('me', [AuthController::class, 'me'])->name('auth.me');
 });
 
 Route::get('/', function() {
