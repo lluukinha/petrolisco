c7c855695a4269fc13a901775043f0e796e7719d
Lucas <lucas.prog07@gmail.com>
2021-12-01 20:45:12 -0300

    controllers created for new migrations/models

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,11 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\Flags\FlagsController;
+use App\Http\Controllers\FuelTypes\FuelTypesController;
+use App\Http\Controllers\GasStationPriceDetails\GasStationPriceDetailsController;
+use App\Http\Controllers\GasStationPrices\GasStationPricesController;
+use App\Http\Controllers\GasStations\GasStationsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,6 +15,12 @@ Route::middleware('api')->prefix('auth')->group(function () {
     Route::post('refresh', [AuthController::class, 'refresh'])->name('auth.refresh');
     Route::get('me', [AuthController::class, 'me'])->name('auth.me');
 });
+
+Route::resource('flags', FlagsController::class);
+Route::resource('fuel-types', FuelTypesController::class);
+Route::resource('gas-stations', GasStationsController::class);
+Route::resource('gas-station-prices', GasStationPricesController::class);
+Route::resource('gas-station-price-details', GasStationPriceDetailsController::class);
 
 Route::get('/', function() {
     return ['text' => 'get endpoint'];
